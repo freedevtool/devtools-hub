114758cd613c136121bdfb5f7099dc321d24ef0e
Free web tools <citybreaks2019@gmail.com>
2026-03-28 17:34:11 +0530

    Update script.js
    New cides 
    
    Signed-off-by: Free web tools <citybreaks2019@gmail.com>

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ async function generateCaption() {
   document.getElementById("captionOutput").innerText =
     data.choices[0].message.content;
 }
+async function paraphraseText() {
+  const input = document.getElementById("paraInput").value;
+
+  const res = await fetch("/api/paraphrase", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ text: input })
+  });
+
+  const data = await res.json();
+
+  document.getElementById("paraOutput").innerText = data.result;
+    }
