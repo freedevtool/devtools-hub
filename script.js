c6a123177d0af366d6d91965c91504076c82a421
Free web tools <citybreaks2019@gmail.com>
2026-03-29 21:18:08 +0530

    Update script.js
    Signed-off-by: Free web tools <citybreaks2019@gmail.com>

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://ai-tools-backend.onrender.com";
+const API_URL = "https://ai-tools-backend-wkam.onrender.com";
 
 async function generateCaption() {
   const input = document.getElementById("captionInput").value;
